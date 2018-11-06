6b3c2871c6e3ef352634ea8c4f4e6364b39d8569
Sameer Ahmed Khan <sakhancse@gmail.com>
2018-11-06 23:11:18 +0530

    adding activate event

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 
 var cacheID = 'mws-restaurant-001'
 
+const expectedCaches = ['mws-restaurant-001'];
+
 const cacheFiles = [
     '/',
     '/index.html',
@@ -34,14 +36,27 @@ self.addEventListener('install', event => {
     )
 })
 
+self.addEventListener('activate', event => {
+    // delete any caches that aren't in expectedCaches
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+        keys.map(key => {
+            if (!expectedCaches.includes(key)) {
+            return caches.delete(key);
+            }
+        })
+        )).then(() => {
+            console.log('mws-restaurant-001 now ready to handle fetches!');
+        })
+    );
+})
+
 self.addEventListener('fetch', function(e) {
     e.respondWith(
         caches.match(e.request).then(function(response) {
             if(response) {
-                console.log('Found', e.request, ' in cache')
                 return response;
             } else {
-                console.log('Could not find ', e.request, ' in cache, fetching')
                 return fetch(e.request)
                 .then(function(response) {
                     const clonedResponse = response.clone()
